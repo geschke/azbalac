dfef15356d16d9836162ba0b499fe9d071525cb1
Ralf Geschke <ralf@kuerbis.org>
2021-05-26 19:15:59 +0200

    social media button styling, first approach with icon styles, second with svg images, work in progress

--- inc/sections/SectionSocialMediaButtons.php
+++ inc/sections/SectionSocialMediaButtons.php
@@ -116,20 +116,40 @@ class Azbalac_Section_Social_Media_Buttons
         $faType = ($buttonType == 2) ? 'bi-square-fill' : 'bi-circle-fill';
         switch ($buttonSize) {
             case 1:
-                $faSize = ' ';
+                
+                $fontSize = ' ';
                 break;
             case 2:
-                $faSize = ' fa-lg ';
+                
+                $fontSize = "font-size: 2rem!important;";
                 break;
             case 3:
-                $faSize = ' fa-2x ';
+                
+                $fontSize = "font-size: 3rem!important;";
                 break;
         }
         $styleFg = '';
         $styleBg = '';
         
-        $output = sprintf('<a target="_blank" href="%s"><span class="fa-stack %s"><i %s class="fa %s fa-stack-2x innersocialbg"></i><i %s class="fa bi-%s fa-stack-1x  innersocial"></i></span></a>', esc_url($url), $faSize, $styleBg, $faType, $styleFg, $socialIcon);
-     
+        //$output = sprintf('<a target="_blank" href="%s"><span class="fa-stack %s"><i %s class="fa %s fa-stack-2x innersocialbg"></i><i %s class="bi bi-%s fa-stack-1x  innersocial"></i></span></a>', esc_url($url), $faSize, $styleBg, $faType, $styleFg, $socialIcon);
+        $svgName =  'css/icons/svg/' . $socialIcon . '.svg';
+
+        ob_start();
+        get_template_part( $svgName);
+        $svgText = ob_get_clean();
+$foo = '
+
+        <div class="fs-2 mb-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon-fill" viewBox="0 0 16 16">
+<path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zm-6.106 4.5L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"></path>
+</svg>
+        Heading
+      </div>
+';
+        //$output = $svgText;
+        //$output = sprintf('<a target="_blank" href="%s"><i style="%s" class="bi bi-%s border border-primary border-2 rounded p-2 _innersocial"></i></a> ', esc_url($url), $fontSize, $socialIcon);
+        $output = sprintf('<div class="azbalac-social" target="_blank" role="img" href="%s">%s FOO</div> ',  esc_url($url), $svgText);
+     $output .= $foo;
         return $output;
     }
 
